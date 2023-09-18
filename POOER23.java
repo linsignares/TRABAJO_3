7a21ffc3e63f7f60ac0453dfed157bb03dc170c6
linsignares <142268649+linsignares@users.noreply.github.com>
2023-09-18 18:07:36 -0500

    Create POOER23.java

---
+++ POOER23.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooep23;
+
+/**
+ *
+ * @author julianainsignares
+ */
+public class POOEP23 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
