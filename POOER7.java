bb2c29a3a5a2ef7ab9a57fb4a5eac43c72ca3c62
linsignares <142268649+linsignares@users.noreply.github.com>
2023-09-18 17:43:57 -0500

    Create POOER7.java

---
+++ POOER7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooer7;
+
+/**
+ *
+ * @author julianainsignares
+ */
+public class POOER7 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
