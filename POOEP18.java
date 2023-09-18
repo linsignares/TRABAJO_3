276a457ad283d322caf9dbf6bcac3c227a93bc39
linsignares <142268649+linsignares@users.noreply.github.com>
2023-09-18 17:31:58 -0500

    Create POOEP18.java

---
+++ POOEP18.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooep18;
+
+/**
+ *
+ * @author julianainsignares
+ */
+public class POOEP18 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
