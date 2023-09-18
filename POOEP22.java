2a89aab10958c016e5e0e9bb5f00dc904f1baa22
linsignares <142268649+linsignares@users.noreply.github.com>
2023-09-18 18:06:24 -0500

    Create POOEP22.java

---
+++ POOEP22.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooep22;
+
+/**
+ *
+ * @author julianainsignares
+ */
+public class POOEP22 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
